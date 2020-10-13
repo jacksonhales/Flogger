0ea127814310c9328b2fe7758c035cccb6b45850
Jackson Hales <me@jacksonhales.com.au>
2020-10-14 01:23:56 +1100

    Used latest Gradle versions. Added queries to RoutineDao.kt

--- app/src/main/java/com/example/flogger/RoutineDao.kt
+++ app/src/main/java/com/example/flogger/RoutineDao.kt
@@ -1,4 +1,20 @@
 package com.example.flogger
 
-class RoutineDao {
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface RoutineDao {
+    @Query ("SELECT * from routine_table ORDER BY id ASC")
+    fun getRoutines(): LiveData<List<Routine>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(routine: Routine)
+
+    @Query("DELETE FROM routine_table")
+    suspend fun deleteAll()
+
 }
